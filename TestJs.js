b0bdb2f5172434b60f4206c67d6ff1f67b9d7a05
Rani Farhat <ranif95@gmail.com>
2019-03-29 13:53:27 +0300

    Update: Finished Navigation Bar, Fully Functioning

--- TestJs.js
+++ TestJs.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-	// let LSUP = false;
+	let isddmdown = false;
 
 	// function ChngToLSU() {
 	// 	LSUP = true;
@@ -10,8 +10,6 @@ $( document ).ready(function() {
 
 	//SETTING DEFAULTS FOR WEBSITE
 	$(".drop-down-bar").css({height: "0em"});
-	$("#left-NB").css({"background-color":"#9CAAC6","box-shadow":"inset 0px 0px 10px #DEE7EF"});
-	$("#center-NB").css({"background-color":"#000063","box-shadow":"inset 0px 0px 10px #000063"});
 
 	$("#center-NB").click(function() {
 		$( document ).ready(function() {
@@ -35,24 +33,25 @@ $( document ).ready(function() {
 		$("#pop").animate({left: "4em"}, {duration: 500 ,queue: false});
 	});
 
-	$("#right-NB").hover(function() {
-		$(".drop-down-bar").animate({opacity: "1"}, {duration: 300 ,queue: false});
-		$(".drop-down-bar").animate({height: "16em"}, {duration: 600 ,queue: false, easing: "easeOutBack"});
-	}, function() {
-		$(".drop-down-bar").animate({opacity: "0"}, {duration: 460 ,queue: false, easing: "easeInExpo"});
-		$(".drop-down-bar").animate({height: "0em"}, {duration: 500 ,queue: false, easing: "easeInBack"});
-	});
-
-	$("#right-NB").click(function() {
-		$(".drop-down-bar").animate({height: "16em"});
-		// $(".drop-down-bar").animate({height: "toggle"});
-	});
-
-	// $(".drop-down-bar").hover(function() {
+	// $("#right-NB").hover(function() {
+	// 	$(".drop-down-bar").animate({opacity: "1"}, {duration: 300 ,queue: false});
 	// 	$(".drop-down-bar").animate({height: "16em"}, {duration: 600 ,queue: false, easing: "easeOutBack"});
 	// }, function() {
 	// 	$(".drop-down-bar").animate({opacity: "0"}, {duration: 460 ,queue: false, easing: "easeInExpo"});
 	// 	$(".drop-down-bar").animate({height: "0em"}, {duration: 500 ,queue: false, easing: "easeInBack"});
 	// });
+
+	$("#right-NB").click(function() {
+		if(isddmdown == false){
+			isddmdown = true;
+			$(".drop-down-bar").animate({height: "18em"}, {duration: 600 ,queue: false, easing: "easeOutBack"});
+			$(".drop-down-bar").animate({opacity: "1"}, {duration: 400 ,queue: false});
+		}
+		else{
+			isddmdown = false;
+			$(".drop-down-bar").animate({height: "0em"}, {duration: 600 ,queue: false, easing: "easeInBack"});
+			$(".drop-down-bar").animate({opacity: "0"}, {duration: 650 ,queue: false, easing: "easeInQuint"});
+		}
+	});
 
 });
